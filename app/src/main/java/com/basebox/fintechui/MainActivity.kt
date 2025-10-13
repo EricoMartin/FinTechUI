9c54abad22b3d07abcf904099c5b27ca58c6e391
Eric Ibu <ericomartin.0785@gmail.com>
2025-10-13 14:11:43 +0100

    Initial commit

---
+++ app/src/main/java/com/basebox/fintechui/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.basebox.fintechui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.basebox.fintechui.ui.theme.FinTechUITheme
+import com.basebox.fintechui.ui.dashboard.WalletDashboardScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FinTechUITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    WalletDashboardScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FinTechUITheme {
+        Greeting("Android")
+    }
+}
