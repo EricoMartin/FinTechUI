5d264ab271e58c6b84fa642eea166d1a9f56b432
Eric Ibu <ericomartin.0785@gmail.com>
2025-10-15 16:27:07 +0100

    fix: UI issues

--- app/src/main/java/com/basebox/fintechui/ui/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/basebox/fintechui/ui/dashboard/DashboardScreen.kt
@@ -16,8 +16,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.basebox.fintechui.ui.components.ActionButtons
 import com.basebox.fintechui.ui.components.AssetsSection
